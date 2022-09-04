44f864639b812d25e37e0fa14db396e6734e1fbd
AmigoDroid <canalmeuovo36@gmail.com>
2022-09-04 12:31:52 -0300

    update server

--- server.js
+++ server.js
@@ -21,15 +21,18 @@ io.on("connection", (socket) => {
 
   socket.on("join_room", (data) => {
     console.log('Grupo: '+data);
+
     if(db.find(grupoId => grupoId[''+data] == data)){
       console.log('grupo existe');
       socket.join(data);
       socket.to(data).emit('update',db.find(grupoId => grupoId[''+data]))
+      console.log('data: '+db);
     }else{
       grupo[""+data] =[];
       db.push(grupo);
       console.log('grupo não existe');
       socket.join(data);
+      console.log('data: '+db);
     }
   
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
@@ -39,6 +42,7 @@ io.on("connection", (socket) => {
     const {room,author,message,time} = data;
     grupo[""+room].push({author:author,message:message,time:time,sockeId:socket.id});
     socket.to(data.room).emit("receive_message", data);
+    console.log('data: '+db);
   });
 
   socket.on("disconnect", () => {
