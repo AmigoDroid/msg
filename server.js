25300f9da5d5332e2b34be9d4a08306cbe2e407d
AmigoDroid <canalmeuovo36@gmail.com>
2022-09-04 12:02:29 -0300

    teste

--- server.js
+++ server.js
@@ -13,16 +13,31 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+const db =[]
+let grupo = {
+  '':[{autor:'',body:'',data:'',sockeId:''}]
+}
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
-    socket.join(data);
+    const {room,author,message,time} = data;
+    if(db[''+room] == true){
+      socket.join(data.room);
+      socket.to(room).emit('update',db[""+room])
+    }else{
+      grupo[room] =[];
+      db.push(grupo);
+      socket.join(data.room);
+    }
+  
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
   socket.on("send_message", (data) => {
+    const {room,author,message,time} = data;
+    grupo[room].push({author:author,message:message,time:time,sockeId:socket.id});
     socket.to(data.room).emit("receive_message", data);
   });
 
